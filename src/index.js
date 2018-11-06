e36b1fc800ce95d5803af4c1047bf3af1c954508
sizeight <hpretorius@gmail.com>
2018-11-06 13:49:03 +0200

    reusable utils

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import AppIconFontAwesome from './components/AppIconFontAwesome';
 import Loading from './components/Loading';
 
+import * as utils from './utils';
+
 export {
   AppIconFontAwesome,
   Loading,
+  utils,
 };
