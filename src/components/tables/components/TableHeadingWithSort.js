6066ec8353b5330ccecd188664c6a440e34ee488
sizeight <hpretorius@gmail.com>
2018-12-10 07:20:52 +0200

    import AppIconFontAwesome from file path

--- src/components/tables/components/TableHeadingWithSort.js
+++ src/components/tables/components/TableHeadingWithSort.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import AppIconFontAwesome from 'components/AppIconFontAwesome';
+import AppIconFontAwesome from '../../AppIconFontAwesome';
 
 
 const propTypes = {
