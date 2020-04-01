594636c4569f52a7acf81bd64b05cbd1fded5aa1
sizeight <hpretorius@gmail.com>
2020-04-01 21:44:33 +0200

    New TableHeadingWithSort

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import tables from './components/tables';
 import AppIconFontAwesome from './components/AppIconFontAwesome';
 import Loading from './components/Loading';
 import Pagination from './components/Pagination';
+import TableHeadingWithSort from './components/TableHeadingWithSort';
 
 import domWatch from './utils';
 
@@ -11,4 +12,5 @@ export {
   domWatch,
   Loading,
   Pagination,
+  TableHeadingWithSort,
 };
